88d2af68da94ed569c703e70b4b92e9c76759a1b
Kirill Gavrilov <gavvvr@ya.ru>
2020-01-03 16:36:55 +0300

    Pre-penultimate verse test passing

--- src/main/kotlin/Bottles.kt
+++ src/main/kotlin/Bottles.kt
@@ -1,9 +1,8 @@
 class Bottles {
     fun verse(num: Int): String {
-        return """
-        |$num bottles of beer on the wall, $num bottles of beer.
-        |Take one down and pass it around, ${num - 1} bottles of beer on the wall.
-        |""".trimMargin()
+        return """$num bottles of beer on the wall, $num bottles of beer.
+Take one down and pass it around, ${num - 1} bottle${if (num == 2) "" else "s"} of beer on the wall.
+"""
     }
 
     fun verses(from: Int, to: Int): String {
