5a438b52d80aa6e9a25a0ae6cfa0cad88002ee30
Kirill Gavrilov <gavvvr@ya.ru>
2020-01-03 16:40:58 +0300

    Shameless green implementation

--- src/main/kotlin/Bottles.kt
+++ src/main/kotlin/Bottles.kt
@@ -1,55 +1,27 @@
-class Item(var number: Int, private val containerType: String, private val substance: String, private val location: String){
-    fun status(withLocation: Boolean): String {
-        val quantity = when (number) {
-            0 -> "no more"
-            else -> number.toString()
-        }
-        val type = if (number != 1) makePlural(containerType) else containerType
-        val evaluatedLocation = if (withLocation) " $location" else ""
-        return "$quantity $type of $substance$evaluatedLocation"
-    }
-
-    private fun makePlural(word: String): String {
-        return word + 's'
-    }
-}
-
 class Bottles {
-
-    private val versesSeparator = System.lineSeparator()
-
-    fun verses(from: Int, to: Int): String {
-        val item = Item(from, "bottle", "beer", "on the wall")
-        return (from downTo to).joinToString(versesSeparator) { verse(it, item) }
-    }
-
-    fun verse(num: Int, item: Item = Item(num, "bottle", "beer", "on the wall")): String {
-        return makeVerse(item)
-    }
-
-    private fun makeVerse(item: Item): String {
-        val initialSentence = "${item.status(true).capitalize()}, ${item.status(false)}."
-        val whatToDo: String
-        if (item.number == 0) {
-            whatToDo = buy()
-            item.number = 99
-        } else {
-            whatToDo = take(item.number == 1)
-            item.number--
-        }
-        val actionSentence = "$whatToDo, ${item.status(true)}."
-        return "$initialSentence\n$actionSentence\n"
-    }
-
-    private fun take(last: Boolean): String {
-        val takeWhat = if (last) "it" else "one"
-        return "Take $takeWhat down and pass it around"
-    }
-
-    private fun buy():String {
-        return "Go to the store and buy some more"
-    }
 
     fun song() = verses(99, 0)
 
+    fun verses(starting: Int, ending: Int) = starting.downTo(ending).joinToString("\n") { verse(it) }
+
+    fun verse(number: Int): String {
+        return when (number) {
+            0 -> """
+            |No more bottles of beer on the wall, no more bottles of beer.
+            |Go to the store and buy some more, 99 bottles of beer on the wall.
+            |""".trimMargin()
+            1 -> """
+            |1 bottle of beer on the wall, 1 bottle of beer.
+            |Take it down and pass it around, no more bottles of beer on the wall.
+            |""".trimMargin()
+            2 -> """
+            |2 bottles of beer on the wall, 2 bottles of beer.
+            |Take one down and pass it around, 1 bottle of beer on the wall.
+            |""".trimMargin()
+            else -> """
+            |$number bottles of beer on the wall, $number bottles of beer.
+            |Take one down and pass it around, ${number - 1} bottles of beer on the wall.
+            |""".trimMargin()
+        }
+    }
 }
