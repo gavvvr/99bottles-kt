265d55b27df4b0323a87cfb63bdb0c35ba4aab9f
Kirill Gavrilov <gavvvr@ya.ru>
2020-01-03 16:36:15 +0300

    First verse test passing

--- src/main/kotlin/Bottles.kt
+++ src/main/kotlin/Bottles.kt
@@ -1,5 +1,14 @@
 class Bottles {
-    fun verse(num: Int):String {
-        return num.toString()
+    fun verse(num: Int): String {
+        return """
+        |99 bottles of beer on the wall, 99 bottles of beer.
+        |Take one down and pass it around, 98 bottles of beer on the wall.
+        |""".trimMargin()
     }
+
+    fun verses(from: Int, to: Int): String {
+        return (from + to).toString()
+    }
+
+    fun song() = ""
 }
