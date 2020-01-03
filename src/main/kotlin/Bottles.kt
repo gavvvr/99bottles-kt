5a6034cfaef8aa263c82c36f5c62967d269c67e2
Kirill Gavrilov <gavvvr@ya.ru>
2020-01-03 16:38:39 +0300

    Last verse test passing

--- src/main/kotlin/Bottles.kt
+++ src/main/kotlin/Bottles.kt
@@ -1,7 +1,10 @@
 class Bottles {
     fun verse(num: Int): String {
-        val firstSentence = "$num bottle${if (num == 1) "" else "s"} of beer on the wall, $num bottle${if (num == 1) "" else "s"} of beer."
-        val secondSentence: String = when (num) {
+        val firstSentence = when (num) {
+            0 -> "No more bottles of beer on the wall, no more bottles of beer."
+            else -> "$num bottle${if (num == 1) "" else "s"} of beer on the wall, $num bottle${if (num == 1) "" else "s"} of beer."
+        }
+        val secondSentence = when (num) {
             1 -> "Take it down and pass it around, no more bottles of beer on the wall.\n"
             0 -> "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
             else -> "Take one down and pass it around, ${num - 1} bottle${if (num == 2) "" else "s"} of beer on the wall.\n"
