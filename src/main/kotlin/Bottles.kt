397970fc5d34e6938f956d93f293645611854eb6
Kirill Gavrilov <gavvvr@ya.ru>
2020-01-03 16:36:30 +0300

    Second verse test passing

--- src/main/kotlin/Bottles.kt
+++ src/main/kotlin/Bottles.kt
@@ -1,8 +1,8 @@
 class Bottles {
     fun verse(num: Int): String {
         return """
-        |99 bottles of beer on the wall, 99 bottles of beer.
-        |Take one down and pass it around, 98 bottles of beer on the wall.
+        |$num bottles of beer on the wall, $num bottles of beer.
+        |Take one down and pass it around, ${num - 1} bottles of beer on the wall.
         |""".trimMargin()
     }
 
